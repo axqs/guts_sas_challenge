be1771d5cc21ea63d92d7399b04f23a7b727cc3b
2335870F <46572897+2335870F@users.noreply.github.com>
2019-10-19 17:16:31 +0100

    added the commented out datefield

--- lib/add_page.dart
+++ lib/add_page.dart
@@ -107,3 +107,22 @@ class _AddPageState extends State<AddPage> {
 		);
 	}
 }
+/*class BasicDateField extends StatelessWidget {
+  final format = DateFormat("yyyy-MM-dd");
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Text('Basic date field (${format.pattern})'),
+      DateTimeField(
+        format: format,
+        onShowPicker: (context, currentValue) {
+          return showDatePicker(
+              context: context,
+              firstDate: DateTime(1900),
+              initialDate: currentValue ?? DateTime.now(),
+              lastDate: DateTime(2100));
+        },
+      ),
+    ]);
+  }
+}*/
